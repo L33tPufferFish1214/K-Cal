81a8479da5be53c481eb81766c45045aa61587bc
Ali H Abdelmaabood <l33tpufferfish1214@gmail.com>
2026-05-25 20:27:43 -0600

    Load app styles in React diagnostic

--- src/reactDiag.tsx
+++ src/reactDiag.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles.css';
 
 function ReactDiagnostic() {
   return (
@@ -18,4 +19,3 @@ createRoot(document.getElementById('root')!).render(
     <ReactDiagnostic />
   </StrictMode>
 );
-
